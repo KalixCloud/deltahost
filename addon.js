6bd83a244c62d1e17b116cc74e199a13d20de004
KalixCloud <90334083+KalixCloud@users.noreply.github.com>
2022-09-18 09:18:04 -0500

    Update addon.js

--- addon.js
+++ addon.js
@@ -1,1 +1,29 @@
-var _0x22f0=["\x2F\x68\x6F\x6D\x65\x2F\x63\x6F\x6E\x74\x61\x69\x6E\x65\x72\x2F","\x69\x6E\x73\x74\x61\x6C\x6C\x41\x6C\x6C\x41\x64\x64\x6F\x6E\x73"];import  BDSAddonInstaller from 'bds-addon-installer';;let serverPath=_0x22f0[0];let installer= new BDSAddonInstaller(serverPath);installer[_0x22f0[1]]()
+import BDSAddonInstaller from 'bds-addon-installer';
+
+
+let serverPath = '/home/container/';
+let installer = new BDSAddonInstaller(serverPath);
+console.log('  ');
+console.log('  ');
+console.log('  ');
+console.log('  ');
+console.log('  ');
+console.log('  ');
+console.log('_____________________________________       ___________________________________   __');
+console.log('___  __ \__  ____/__  /___  __/__    |      ___    |__  __ \__  __ \_  __ \__  | / /');
+console.log('__  / / /_  __/  __  / __  /  __  /| |      __  /| |_  / / /_  / / /  / / /_   |/ / ');
+console.log('_  /_/ /_  /___  _  /___  /   _  ___ |      _  ___ |  /_/ /_  /_/ // /_/ /_  /|  /  ');
+console.log('/_____/ /_____/  /_____/_/    /_/  |_|      /_/  |_/_____/ /_____/ \____/ /_/ |_/   ');
+console.log('                                                                                    ');
+console.log('____________   ___________________________________________________                  ');
+console.log('____  _/__  | / /_  ___/__  __/__    |__  /___  /___  ____/__  __ \                 ');
+console.log(' __  / __   |/ /_____ \__  /  __  /| |_  / __  / __  __/  __  /_/ /                 ');
+console.log('__/ /  _  /|  / ____/ /_  /   _  ___ |  /___  /___  /___  _  _, _/                  ');
+console.log('/___/  /_/ |_/  /____/ /_/    /_/  |_/_____/_____/_____/  /_/ |_|                   ');
+console.log('  ');
+console.log('Creado por LeonardoOVH | Administrador General | deltahost.cloud');
+console.log('  ');
+console.log('  ');
+console.log('  ');
+console.log('  ');
+installer.installAllAddons();
